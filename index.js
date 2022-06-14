50498859756220574fb07776f5bc7a71651bbf39
dev <dev@dev.com>
2022-06-14 00:12:12 -0700

    .

--- index.js
+++
@@ -1,9 +0,0 @@
-console.log("Hello world!");
-
-function add(a, b) {
-  return a + b
-}
-
-module.exports = {
-  add
-}
---
+++ server.js
@@ -0,0 +1,3 @@
+// const Mocha = require('./test/test.js')
+
+console.log("hello");
